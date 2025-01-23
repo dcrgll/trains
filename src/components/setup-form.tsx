bad2213f5454923e5ba654a56d59f9e9e0ce1e70
Dan Cargill <dan@cargill.dev>
2025-01-23 00:18:17 +0000

    Add loader

--- src/components/setup-form.tsx
+++ src/components/setup-form.tsx
@@ -3,21 +3,12 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Check, ChevronsUpDown } from 'lucide-react'
+import { Loader2 } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
 import { stations } from '@/lib/stations'
-import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList
-} from '@/components/ui/command'
 import {
   Form,
   FormControl,
@@ -26,37 +17,35 @@ import {
   FormLabel,
   FormMessage
 } from '@/components/ui/form'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger
-} from '@/components/ui/popover'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 
+import Select from './select'
+
 const formSchema = z.object({
-  station: z.string().nonempty(),
+  station: z.object({
+    label: z.string(),
+    value: z.string()
+  }),
   board: z.enum(['departures', 'arrivals', 'all'])
 })
 
 export default function SetupForm() {
   const router = useRouter()
-  const [isCommandOpen, setIsCommandOpen] = useState(false)
+
+  const [loading, setLoading] = useState(false)
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema)
   })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
+    setLoading(true)
     try {
-      router.push(`/${values.station.toLowerCase()}/${values.board}`)
+      router.push(`/${values.station.value.toLowerCase()}/${values.board}`)
     } catch (error) {
       console.error('Form submission error', error)
     }
   }
 
-  function handleSelectStation(station: string) {
-    form.setValue('station', station)
-    setIsCommandOpen(false)
-  }
   return (
     <Form {...form}>
       <form
@@ -64,62 +53,19 @@ export default function SetupForm() {
         className="mx-auto max-w-3xl space-y-8"
       >
         <FormField
-          control={form.control}
           name="station"
+          control={form.control}
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
               <FormLabel>Station</FormLabel>
-              <Popover open={isCommandOpen} onOpenChange={setIsCommandOpen}>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        'w-full justify-between',
-                        !field.value && 'text-muted-foreground'
-                      )}
-                    >
-                      {field.value
-                        ? stations.find(
-                            (station) => station.crsCode === field.value
-                          )?.stationName
-                        : 'Select station'}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[340px] p-0 lg:w-[400px]">
-                  <Command>
-                    <CommandInput placeholder="Search station..." />
-                    <CommandList>
-                      <CommandEmpty>No station found.</CommandEmpty>
-                      <CommandGroup>
-                        {stations.map((station) => (
-                          <CommandItem
-                            value={station.stationName}
-                            key={station.crsCode}
-                            onSelect={() => {
-                              handleSelectStation(station.crsCode)
-                            }}
-                          >
-                            <Check
-                              className={cn(
-                                'mr-2 h-4 w-4',
-                                station.crsCode === field.value
-                                  ? 'opacity-100'
-                                  : 'opacity-0'
-                              )}
-                            />
-                            {station.stationName}
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-
+              <Select
+                {...field}
+                placeholder="Select station..."
+                options={stations.map((station) => ({
+                  label: station.stationName,
+                  value: station.crsCode
+                }))}
+              />
               <FormMessage />
             </FormItem>
           )}
@@ -166,8 +112,13 @@ export default function SetupForm() {
             </FormItem>
           )}
         />
-        <Button type="submit" className="w-full">
-          Submit
+
+        <Button type="submit" className="w-full" disabled={loading}>
+          {loading ? (
+            <Loader2 className="h-5 w-5 animate-spin text-foreground" />
+          ) : (
+            'Submit'
+          )}
         </Button>
       </form>
     </Form>
